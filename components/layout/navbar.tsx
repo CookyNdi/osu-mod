e9be6b66221e7b35e20f6bf44e912a43e22f4f94
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-05-25 10:28:18 +0700

    feat : main layout

---
+++ components/layout/navbar.tsx
@@ -0,0 +1,13 @@
+import Profile from './profile';
+
+export default function Navbar() {
+  return (
+    <div className='w-full h-[60px] bg-background/20 backdrop-blur-sm flex justify-between items-center p-2 px-8 fixed border-b border-primary/50'>
+      <h1>Osu Modding</h1>
+      <div className='flex items-center gap-x-6'>
+        <p className='cursor-pointer'>My Request</p>
+        <Profile />
+      </div>
+    </div>
+  );
+}
---
+++ components/layout/profile.tsx
@@ -0,0 +1,29 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+export default function Profile() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className='cursor-pointer'>
+          <AvatarImage src='https://a.ppy.sh/16983379?1677463087.jpeg' />
+          <AvatarFallback>C</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56 z-[120]' align='end'>
+        <div>
+          <DropdownMenuLabel>CookyNdi</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className='cursor-pointer'>Log out</DropdownMenuItem>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
