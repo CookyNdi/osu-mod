eba5d3d34a38f5fb3abbdf03c6420e6a13bb0256
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-05-30 11:19:51 +0700

    feat : fill data in archived page

---
+++ app/(main)/[username]/archives/_components/content.tsx
@@ -0,0 +1,36 @@
+import { Session } from 'next-auth';
+
+import { RequestData } from '@/types/request';
+import ModdingCard from '@/components/modding-card';
+
+type ArchiveContentProps = {
+  request: RequestData[];
+  session: Session | null;
+};
+
+export default function ArchiveContent({ request, session }: ArchiveContentProps) {
+  return (
+    <div className='space-y-2'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-xl font-semibold'>Archives</h1>
+        <p className='text-muted-foreground text-sm'>Total : {request.length}</p>
+      </div>
+      {request.length < 1 ? (
+        <div className='w-full pt-8 flex justify-center items-center'>
+          <h1 className='capitalize'>Modder haven&apos;t get any request yet.</h1>
+        </div>
+      ) : (
+        <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
+          {request.map((data) => (
+            <ModdingCard
+              request={data}
+              key={data.id}
+              isModderPage
+              isEditable={session?.user.name === request[0].user.username}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
--- app/(main)/[username]/archives/page.tsx
+++ app/(main)/[username]/archives/page.tsx
@@ -1,19 +1,21 @@
 import Footer from '@/components/layout/footer';
-import ModdingCard from '@/components/modding-card';
+import ArchiveContent from './_components/content';
+import { getSession } from '@/lib/session';
+import { getAllArchiveRequest } from '@/actions/request/get-all-archive-request';
 
-export default function ArchivesPage() {
+type ArchivesPageProps = {
+  params: {
+    username: string;
+  };
+};
+
+export default async function ArchivesPage({ params }: ArchivesPageProps) {
+  const username = params.username || '';
+  const session = await getSession();
+  const request = await getAllArchiveRequest(username);
   return (
     <div className='flex flex-col gap-y-4'>
-      <div className='flex justify-between items-center'>
-        <h1 className='text-xl font-semibold'>Archives</h1>
-        <p className='text-muted-foreground text-sm'>Total : 2</p>
-      </div>
-      <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
-        <ModdingCard isEditable />
-        <ModdingCard isEditable />
-        <ModdingCard isEditable />
-        <ModdingCard isEditable />
-      </div>
+      <ArchiveContent request={request} session={session} />
       <Footer />
     </div>
   );
