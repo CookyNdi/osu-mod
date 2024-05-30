18b1d60c6e2d4489db5198c5cf6a5945bde19ef9
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-05-30 11:19:35 +0700

    feat : fill data in completed page

---
+++ actions/request/get-all-modded-request.ts
@@ -0,0 +1,45 @@
+'use server';
+
+import { db } from '@/lib/db';
+import { getUserByUsername } from '@/data/users';
+
+export const getAllModdedRequest = async (username: string) => {
+  try {
+    const user = await getUserByUsername(username);
+    if (!user) return [];
+    const request = await db.request.findMany({
+      where: { targetUserId: user.id, status: 'MODDED' },
+      orderBy: { createdAt: 'desc' },
+      include: {
+        user: {
+          select: {
+            id: true,
+            username: true,
+            avatar_url: true,
+            Settings: { select: { modder_type: true } },
+          },
+        },
+        beatmap: {
+          select: {
+            id: true,
+            title: true,
+            artist: true,
+            bpm: true,
+            creator: true,
+            cover_url: true,
+            length: true,
+            BeatmapSetDiff: {
+              select: { id: true, beatmapId: true, difficulty_rating: true, mode: true, version: true },
+            },
+          },
+        },
+      },
+    });
+
+    return request;
+  } catch (error) {
+    console.log(error);
+    console.log('getAllMyRequest ISSUE');
+    return [];
+  }
+};
--- app/(main)/[username]/completed/page.tsx
+++ app/(main)/[username]/completed/page.tsx
@@ -1,20 +1,21 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import Footer from '@/components/layout/footer';
-import ModdingCard from '@/components/modding-card';
+import CompletedContent from './_components/content';
+import { getSession } from '@/lib/session';
+import { getAllModdedRequest } from '@/actions/request/get-all-modded-request';
 
-export default function CompletedPage() {
+type CompletedPageProps = {
+  params: {
+    username: string;
+  };
+};
+
+export default async function CompletedPage({ params }: CompletedPageProps) {
+  const username = params.username || '';
+  const session = await getSession();
+  const request = await getAllModdedRequest(username);
   return (
     <div className='flex flex-col gap-y-4'>
-      <div className='flex justify-between items-center'>
-        <h1 className='text-xl font-semibold'>Completed</h1>
-        <p className='text-muted-foreground text-sm'>Total : 24</p>
-      </div>
-      <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
-        <ModdingCard isEditable />
-        <ModdingCard isEditable />
-        <ModdingCard isEditable />
-        <ModdingCard isEditable />
-      </div>
+      <CompletedContent request={request} session={session} />
       <Footer />
     </div>
   );
