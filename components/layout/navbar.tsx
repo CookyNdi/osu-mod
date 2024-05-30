a307664e1603c457458b434ab4a572c61e93e825
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-05-30 12:14:56 +0700

    feat : hide my request if not loggedin

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -10,9 +10,11 @@ export default async function Navbar() {
     <div className='w-full h-[60px] bg-background/20 backdrop-blur-sm flex justify-between items-center p-2 px-4 lg:px-8 fixed z-[100] border-b border-primary/50'>
       <Link href={'/'}>Osu Modding</Link>
       <div className='flex items-center gap-x-4 lg:gap-x-6'>
-        <Link href={'/request'} className='cursor-pointer'>
-          My Request
-        </Link>
+        {session && (
+          <Link href={'/request'} className='cursor-pointer'>
+            My Request
+          </Link>
+        )}
         {session ? (
           <Profile session={session} />
         ) : (
