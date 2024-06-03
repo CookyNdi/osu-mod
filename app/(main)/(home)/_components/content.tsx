e3182d664ab598268338f70231db85cab3b98158
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-06-03 09:43:52 +0700

    feat : display request button in home card

--- app/(main)/(home)/_components/content.tsx
+++ app/(main)/(home)/_components/content.tsx
@@ -1,10 +1,12 @@
 'use client';
 import Link from 'next/link';
 import { formatDistanceToNow } from 'date-fns';
+import { Session } from 'next-auth';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Users } from '@prisma/client';
+import FormRequest from '@/components/form-request';
 
 type HomeContentsProps = {
   users: (Users & {
@@ -14,30 +16,37 @@ type HomeContentsProps = {
       modder_type: string;
     }[];
   })[];
+  session: Session | null;
 };
 
-export default function HomeContents({ users }: HomeContentsProps) {
+export default function HomeContents({ users, session }: HomeContentsProps) {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-4 gap-4'>
       {users.map((data) => (
         <Card key={data.id}>
-          <CardHeader>
-            <div className='flex justify-between items-center'>
-              <CardTitle className='truncate'>{data.username}</CardTitle>
-              <p>{data.Settings[0].open ? 'Open' : 'Closed'}</p>
-            </div>
-            <CardDescription className='flex flex-col'>
-              <span className='text-foreground capitalize'>{data.Settings[0].modder_type}</span>
-              <span className='text-xs'>{`Last Seen ${formatDistanceToNow(data.last_action, {
-                addSuffix: true,
-                includeSeconds: true,
-              })}`}</span>
-            </CardDescription>
-          </CardHeader>
+          <Link href={`/${data.username}`}>
+            <CardHeader>
+              <div className='flex justify-between items-center'>
+                <CardTitle className='truncate'>{data.username}</CardTitle>
+                <p>{data.Settings[0].open ? 'Open' : 'Closed'}</p>
+              </div>
+              <CardDescription className='flex flex-col'>
+                <span className='text-foreground capitalize'>{data.Settings[0].modder_type}</span>
+                <span className='text-xs'>{`Last Seen ${formatDistanceToNow(data.last_action, {
+                  addSuffix: true,
+                  includeSeconds: true,
+                })}`}</span>
+              </CardDescription>
+            </CardHeader>
+          </Link>
           <CardContent className='flex justify-end'>
-            <Link href={`/${data.username}`}>
-              <Button>Queue</Button>
-            </Link>
+            {data.username !== session?.user.name && (
+              <FormRequest targetUserId={data.id} username={data.username}>
+                <Button disabled={!!!data.Settings[0].open || !session} className='disabled:cursor-not-allowed'>
+                  Request
+                </Button>
+              </FormRequest>
+            )}
           </CardContent>
         </Card>
       ))}
--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -1,13 +1,21 @@
 import { getAllActiveModders } from '@/actions/user/get-all-active-modders';
 import HomeContents from './_components/content';
 import HomeContentFilters from './_components/filters';
+import { getSession } from '@/lib/session';
 
 export default async function Home() {
+  const session = await getSession();
   const users = await getAllActiveModders();
   return (
     <div className='w-full flex flex-col gap-y-4 pt-4'>
       <HomeContentFilters />
-      <HomeContents users={users} />
+      {users.length < 1 ? (
+        <div className='flex justify-center items-center'>
+          <h1 className='text-lg text-muted-foreground'>there`s nobody here.</h1>
+        </div>
+      ) : (
+        <HomeContents users={users} session={session} />
+      )}
     </div>
   );
 }
