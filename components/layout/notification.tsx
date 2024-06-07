04169e798bfc969c8c292e3ca8e1455ac677d89c
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-06-07 09:19:06 +0700

    feat : adding scroll area in notification menu

--- components/layout/notification.tsx
+++ components/layout/notification.tsx
@@ -15,6 +15,7 @@ import {
 import { notifications } from '@/types/notification';
 import { readNotification } from '@/actions/notification/read-notification';
 import { bulkReadNotification } from '@/actions/notification/bulk-read-notification';
+import { ScrollArea } from '../ui/scroll-area';
 
 type NotificationProps = {
   notifications: notifications[];
@@ -53,50 +54,55 @@ export default function Notification({ notifications }: NotificationProps) {
           </p>
         </div>
         <DropdownMenuSeparator />
-        {notifications.length < 1 ? (
-          <div className='mt-4 flex justify-center items-center'>
-            <h1 className='text-sm text-muted-foreground'>Notification Is Empty!</h1>
-          </div>
-        ) : (
-          <>
-            {notifications.map((data) => (
-              <div className='flex mt-4 group bg-muted/50 rounded-md p-0 py-0 space-x-2 overflow-hidden' key={data.id}>
-                <div className='relative w-[25%] h-20 overflow-hidden'>
-                  <div className='absolute bg-background/20 top-0 left-0 z-20 w-full h-full' />
-                  <Image
-                    className='object-cover z-10'
-                    src={data.request.beatmap.cover_url}
-                    alt={data.request.beatmap.title}
-                    fill
-                  />
-                </div>
-                <div className='w-[75%] flex justify-between items-center'>
-                  <div className='flex flex-col py-2'>
-                    <p className='text-sm'>
-                      {data.type === 'CHANGE_STATUS'
-                        ? `Beatmap ${data.request.beatmap.title} ${data.message} ${data.request.status}`
-                        : `${data.message} ${data.request.beatmap.title}`}
-                    </p>
-                    <p className='text-muted-foreground text-sm'>
-                      {formatDistanceToNow(data.createdAt, {
-                        addSuffix: true,
-                        includeSeconds: true,
-                      })}
-                    </p>
-                  </div>
-                  <div className='w-[15%]'>
-                    <FaCheck
-                      size={24}
-                      className='cursor-pointer hidden group-hover:block'
-                      onClick={() => markAsRead(data.id)}
-                      title='mark as read'
+        <ScrollArea className='w-full h-[288px]'>
+          {notifications.length < 1 ? (
+            <div className='mt-4 flex justify-center items-center'>
+              <h1 className='text-sm text-muted-foreground'>Notification Is Empty!</h1>
+            </div>
+          ) : (
+            <>
+              {notifications.map((data) => (
+                <div
+                  className='flex mt-4 group bg-muted/50 rounded-md p-0 py-0 space-x-2 overflow-hidden'
+                  key={data.id}
+                >
+                  <div className='relative w-[25%] h-20 overflow-hidden'>
+                    <div className='absolute bg-background/20 top-0 left-0 z-20 w-full h-full' />
+                    <Image
+                      className='object-cover z-10'
+                      src={data.request.beatmap.cover_url}
+                      alt={data.request.beatmap.title}
+                      fill
                     />
                   </div>
+                  <div className='w-[75%] flex justify-between items-center'>
+                    <div className='flex flex-col py-2'>
+                      <p className='text-sm'>
+                        {data.type === 'CHANGE_STATUS'
+                          ? `Beatmap ${data.request.beatmap.title} ${data.message} ${data.request.status}`
+                          : `${data.message} ${data.request.beatmap.title}`}
+                      </p>
+                      <p className='text-muted-foreground text-sm'>
+                        {formatDistanceToNow(data.createdAt, {
+                          addSuffix: true,
+                          includeSeconds: true,
+                        })}
+                      </p>
+                    </div>
+                    <div className='w-[15%]'>
+                      <FaCheck
+                        size={24}
+                        className='cursor-pointer hidden group-hover:block'
+                        onClick={() => markAsRead(data.id)}
+                        title='mark as read'
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </>
-        )}
+              ))}
+            </>
+          )}
+        </ScrollArea>
       </DropdownMenuContent>
     </DropdownMenu>
   );
