582d958e8c7d02df8b537be41f622d3241dea4ed
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-06-07 09:30:02 +0700

    feat : adding icon in profile menu

--- components/layout/profile.tsx
+++ components/layout/profile.tsx
@@ -1,6 +1,10 @@
 'use client';
 import Link from 'next/link';
 import { Session } from 'next-auth';
+import { FaUser } from 'react-icons/fa';
+import { MdOutlineNoteAlt } from 'react-icons/md';
+import { IoSettingsSharp } from 'react-icons/io5';
+import { BiLogOut } from 'react-icons/bi';
 
 import {
   DropdownMenu,
@@ -34,16 +38,26 @@ export default function Profile({ session }: ProfileProps) {
           <DropdownMenuLabel>{session.user?.name || ''}</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <Link href={`/${session.user.name}`}>
-            <DropdownMenuItem className='cursor-pointer'>Your Page</DropdownMenuItem>
+            <DropdownMenuItem className='cursor-pointer grid items-center grid-cols-10'>
+              <FaUser size={19} className='col-span-2' />
+              <span className='col-span-8'>Your Page</span>
+            </DropdownMenuItem>
           </Link>
-          <Link href={"/rules"}>
-            <DropdownMenuItem className='cursor-pointer'>Edit Rules</DropdownMenuItem>
+          <Link href={'/rules'}>
+            <DropdownMenuItem className='cursor-pointer grid items-center grid-cols-10'>
+              <MdOutlineNoteAlt size={20} className='col-span-2' />
+              <span className='col-span-8'>Edit Rules</span>
+            </DropdownMenuItem>
           </Link>
           <Link href={'/settings'}>
-            <DropdownMenuItem className='cursor-pointer'>Settings</DropdownMenuItem>
+            <DropdownMenuItem className='cursor-pointer grid items-center grid-cols-10'>
+              <IoSettingsSharp size={19} className='col-span-2' />
+              <span className='col-span-8'>Settings</span>
+            </DropdownMenuItem>
           </Link>
-          <DropdownMenuItem className='cursor-pointer' onClick={onClick}>
-            Log out
+          <DropdownMenuItem className='cursor-pointer grid items-center grid-cols-10' onClick={onClick}>
+            <BiLogOut size={20} className='col-span-2' />
+            <span className='col-span-8'>Log out</span>
           </DropdownMenuItem>
         </div>
       </DropdownMenuContent>
